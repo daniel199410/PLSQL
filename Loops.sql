ef40fa39064b8ad5a4e07c0e3fa499d961999f87
Daniel Cataño <dcatano@cidenet.com.co>
2018-12-21 08:48:03 -0500

    Loop task

--- Loops.sql
+++ Loops.sql
@@ -1,7 +1,11 @@
 SET SERVEROUTPUT ON;
 DECLARE
+  TYPE notesArray IS VARRAY(4) OF NUMBER;
+  notes notesArray := notesArray(4.25, 3, 3.75, 4.25);
   x NUMBER := 1;
   y NUMBER := 1;
+  accSum NUMBER := 0;
+  total NUMBER := 0;
 BEGIN
   <<loop_1>>
   FOR i IN 1..10 LOOP
@@ -23,5 +27,11 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(y || '*2=' || (y * 2));
     y := y + 1;
   END LOOP loop_3;
+  
+  FOR i IN 1..notes.COUNT LOOP
+    accSum := accSum + notes(i);
+  END LOOP;
+  total := accSum / notes.COUNT;
+  DBMS_OUTPUT.PUT_LINE('avg: ' || total);
 END;
 /
